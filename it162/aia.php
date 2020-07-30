8f451e416ed4a1560dd3e85fbc83fe78038ba499
CNavo <52547964+CNavo@users.noreply.github.com>
2020-07-30 15:06:48 -0700

    07/30/2020
    Added aia.php and aia.css forms

--- it162/aia.php
+++ it162/aia.php
@@ -1,17 +1,141 @@
 <?php include('includes/header.php');?>
+<link rel="stylesheet" href="css/aia.css"/>
 
-  <body> 
-  <main>
+<!-- Table 1--------------------------------------------------- -->
+
+<h2>Client Information</h2>
+
+<table class="clientInfo">
+<tbody>
+<tr>
+<td>Client's name and final decision maker:</td>
+<td>Eric Lloyd</td>
+</tr>
+<tr>
+<td>Client's Business Webite Name:</td>
+<td>Twin Eclipse Books</td>
+</tr>
+<tr>
+<td>Redesign:</td>
+<td><a href="http://twineclipsebooks.com/" target="_blank">twineclipsebooks.com</a></td>
+</tr>
+<tr>
+<td>Type of Website:</td>
+<td>Identity, Informational, Shopping, Entertainment </td>
+</tr>
+<tr>
+<td>Client's Goals:</td>
+<td>To generate more sales of current published book.  To achieve a wider online presence. 
+      And provide a location to reference ideas for readers to get excited about.</td>
+</tr>
+</tbody>
+</table>
+
+<!-- Table 2-------------------------------------------------- -->
+
+<h2> Audience Information </h2>
+
+<table class="aiaInfo">
+<thead>
+<tr>
+<th>Audience</th>
+<th>Issues, Concerns, & Needs</th>
+<th>Approach</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>New Visitors to the Site:
+    Scenario:   
+    Should have a Mac or PC computer, tablet or mobile device 
+     with internet access. The Site will be optimized for any search engine.
+     Avg Screen resolution: 2880 x 1880. The site is welcomed to men and women 
+     with common interests. There is no fee for using this site.  
+</td>
+<td>
+  <ul style="list-style-type:disc;">
+    <li>Access to new reads in SciFi/fantasy fiction as well as
+        Recovery and self-help nonfiction with selling prices included</li>
+    <li>Easy to navigate menu bar to specific parts of the site</li>
+    <li>Able to see a bio about the author</li>
+    <li>Freshly updated title page with quotes or upcoming material</li>
+    <li>Link for any questions or concerns</li> 
+  </ul> 
+</td>
+<td>
+  <ul style="list-style-type:disc;">
+    <li>Direct Link to Amazon.com for each book to purchase. Available in physical copy or ebook</li>
+    <li>Main Nav bar to seperate Recovery/Addiction and SciFi/Fantasy portions.</li>
+    <li>Product descriptions for each product for sale</li>
+    <li>About the Author page</li>
+    <li>General Contact Form</li>
+    <li>Links to social media accounts</li>  
+  </ul>
+</td>
+</tr>
+
+<tr>
+<td>People in Recovery: 
+    Scenario:
+    Visitors should have a Mac or PC computer, tablet or mobile device 
+    with internet access. The Site will be optimized for any search engine.
+    The site is welcomed to men and women recovering from addiction 
+    and seeking further outlets to recovery. Due to discussion of addiction 
+    thats sometimes explicit in content, age identification will be required 
+    for this portion of the website. There is no fee for using this portion of the site.
+</td>
+<td>
+  <ul style="list-style-type:disc;">
+    <li>What products are available and how much.</li>
+    <li>Visitors who seek help on the Recovery page may want to be anonymous</li>
+    <li>Secure access to the page based on age identification</li>
+    <li>Access to Additional help</li>
+  </ul>  
+</td>
+<td>
+  <ul style="list-style-type:disc;">
+    <li>Direct Link to Amazon.com for each book to purchase. Available in physical copy or ebook</li>
+    <li>A message board to where concerns about addiction and be answered by author
+        and/or peers without having to give any personal information.</li> 
+    <li>To access this page. A warning requiring you to be 18 and entering a page
+        with sensitive material.</li>
+    <li>Links to help find localized recovery meetings near the visitor.</li>
+    <li>General Contact Form</li>
+</td>
+</tr>
+
+<tr>
+<td>Science Fiction & Fantasy enthusiasts:
+    Scenario:  
+    Visitors should have a Mac or PC computer, tablet or mobile device 
+    with internet access. The Site will be optimized for any search engine.
+    Avg Screen resolution: 2880 x 1880. The site is welcomed to men 
+    and women, inspiring writers, and love of SciFi & Fantasy genres.
+    There is no fee for using this portion of the site site.
+</td>
+<td>
+  <ul style="list-style-type:disc;"> 
+    <li>What products are available and how much.</li>
+    <li>Interactive details of the books and the creation process.</li>
+    <li>Ability to access on all devices.</li>
+    <li>Ability to provide discussion, thoughts, and theories about the book content.</li>   
+  </ul>
+</td>
+<td>
+  <ul style="list-style-type:disc;">
+    <li>Direct Link to Amazon.com for each book to purchase. Available in physical copy or ebook</li>
+    <li>Synopsis for what each book is about, including pictures</li>
+    <li>Interactive introduction to the world created based on the books</li>
+    <li>Links to getting into SciFi and Fantasy writing and other references</li>
+    <li>A forum open to discussion and theories</li>
+    <li>General Contact Form</li>  
+  </ul>
+</td>
+</tr>
+</tbody>
+</table>
 
 
+<?php include('includes/footer.php');?>  
 
-
-   <?php include('includes/footer.php');?>  
-
-  </main>
-
-  <?php include('includes/script.php');?>   
-
-     
- </body>
-</html>   
+ 
